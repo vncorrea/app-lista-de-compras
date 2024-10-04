d3bbdb62acc82856884133d05a669e73d49f1d1f
ArthurRqR <Arthurrcampos115@gmail.com>
2024-10-04 14:01:01 -0300

    Algumas mudanças de organizações, adicionei algumas coisas no manifest e  fiz a parte de UI do CreateList e Lists

--- app/src/main/java/com/example/lista_de_compras/data/model/ShoppingList.kt
+++ app/src/main/java/com/example/lista_de_compras/data/model/ShoppingList.kt
@@ -1,3 +1,4 @@
 data class ShoppingList(
-    var title: String
+    val name: String,      // Nome da lista (ex: "Lista do Mercado")
+    val items: MutableList<String> = mutableListOf()  // Itens da lista de compras
 )
